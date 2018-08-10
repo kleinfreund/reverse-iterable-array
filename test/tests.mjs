52f0edaee8fc9db6f99352dd1dbb055d7f2c245a
kleinfreund <phrudloff@gmail.com>
2018-08-10 10:43:04 +0200

    Separate test runner code from test cases

--- test/tests.mjs
+++ test/tests.mjs
@@ -1,204 +1,172 @@
 import { ReverseIterableArray } from '../src/reverse-iterable-array.mjs';
+import { TestRunner } from './test-runner.mjs';
 
-let totalAssertions = 0;
-let correctAssertions = 0;
+function tests() {
+  const testRunner = new TestRunner();
+  console.group('Tests');
+  console.info('Running tests …');
 
-function assert(condition, message) {
-  totalAssertions++;
+  const array = new ReverseIterableArray('1', '2', '3');
 
-  if (condition) {
-    correctAssertions++;
-  }
+  console.group('array[]');
+  testRunner.assertEqual('array.length', 3, array.length);
+  testRunner.assertEqual('array[0]', '1', array[0]);
+  testRunner.assertEqual('array[1]', '2', array[1]);
+  testRunner.assertEqual('array[2]', '3', array[2]);
+  console.groupEnd();
 
-  console.assert(condition, message);
+  array.splice(0, array.length);
+
+  console.group('Iterables & Iterators');
+  array.push('4', '5', '6');
+
+  console.group('array');
+  testRunner.assertHasOwnProperty('ReverseIterableArray.prototype', Symbol.iterator, ReverseIterableArray.prototype);
+  console.groupEnd();
+
+  console.group('array.entries()');
+  const entries = array.entries();
+  console.info('> const entries = array.entries();');
+  testRunner.assertHasOwnProperty('entries', 'next', entries);
+  testRunner.assertHasOwnProperty('entries', Symbol.iterator, entries);
+
+  let entriesNext = entries.next();
+  console.info('> let entriesNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesNext', 'done', entriesNext);
+  testRunner.assertHasOwnProperty('entriesNext', 'value', entriesNext);
+  testRunner.assertEqual('entriesNext.done', false, entriesNext.done);
+  testRunner.assertEqual('entriesNext.value[0]', 0, entriesNext.value[0]);
+  testRunner.assertEqual('entriesNext.value[1]', '4', entriesNext.value[1]);
+
+  entriesNext = entries.next();
+  console.info('> entriesNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesNext', 'done', entriesNext);
+  testRunner.assertHasOwnProperty('entriesNext', 'value', entriesNext);
+  testRunner.assertEqual('entriesNext.done', false, entriesNext.done);
+  testRunner.assertEqual('entriesNext.value[0]', 1, entriesNext.value[0]);
+  testRunner.assertEqual('entriesNext.value[1]', '5', entriesNext.value[1]);
+
+  entriesNext = entries.next();
+  console.info('> entriesNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesNext', 'done', entriesNext);
+  testRunner.assertHasOwnProperty('entriesNext', 'value', entriesNext);
+  testRunner.assertEqual('entriesNext.done', false, entriesNext.done);
+  testRunner.assertEqual('entriesNext.value[0]', 2, entriesNext.value[0]);
+  testRunner.assertEqual('entriesNext.value[1]', '6', entriesNext.value[1]);
+
+  entriesNext = entries.next();
+  console.info('> entriesNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesNext', 'done', entriesNext);
+  testRunner.assertEqual('entriesNext.done', true, entriesNext.done);
+  console.groupEnd();
+
+  console.group('array.keys()');
+  const keys = array.keys();
+  console.info('> const keys = array.keys();');
+  testRunner.assertHasOwnProperty('keys', 'next', keys);
+  testRunner.assertHasOwnProperty('keys', Symbol.iterator, keys);
+
+  let keysNext = keys.next();
+  console.info('> let keysNext = keys.next();');
+  testRunner.assertHasOwnProperty('keysNext', 'done', keysNext);
+  testRunner.assertHasOwnProperty('keysNext', 'value', keysNext);
+  testRunner.assertEqual('keysNext.value', 0, keysNext.value);
+  testRunner.assertEqual('keysNext.done', false, keysNext.done);
+
+  keysNext = keys.next();
+  console.info('> keysNext = keys.next();');
+  testRunner.assertHasOwnProperty('keysNext', 'done', keysNext);
+  testRunner.assertHasOwnProperty('keysNext', 'value', keysNext);
+  testRunner.assertEqual('keysNext.value', 1, keysNext.value);
+  testRunner.assertEqual('keysNext.done', false, keysNext.done);
+
+  keysNext = keys.next();
+  console.info('> keysNext = keys.next();');
+  testRunner.assertHasOwnProperty('keysNext', 'done', keysNext);
+  testRunner.assertHasOwnProperty('keysNext', 'value', keysNext);
+  testRunner.assertEqual('keysNext.value', 2, keysNext.value);
+  testRunner.assertEqual('keysNext.done', false, keysNext.done);
+
+  keysNext = keys.next();
+  console.info('> keysNext = keys.next();');
+  testRunner.assertHasOwnProperty('keysNext', 'done', keysNext);
+  testRunner.assertEqual('keysNext.done', true, keysNext.done);
+  console.groupEnd();
+
+  console.group('array.values()');
+  const values = array.values();
+  console.info('> const values = array.values();');
+  testRunner.assertHasOwnProperty('values', 'next', values);
+  testRunner.assertHasOwnProperty('values', Symbol.iterator, values);
+
+  let valuesNext = values.next();
+  console.info('> let valuesNext = values.next();');
+  testRunner.assertHasOwnProperty('valuesNext', 'done', valuesNext);
+  testRunner.assertHasOwnProperty('valuesNext', 'value', valuesNext);
+  testRunner.assertEqual('valuesNext.value', '4', valuesNext.value);
+  testRunner.assertEqual('valuesNext.done', false, valuesNext.done);
+
+  valuesNext = values.next();
+  console.info('> valuesNext = values.next();');
+  testRunner.assertHasOwnProperty('valuesNext', 'done', valuesNext);
+  testRunner.assertHasOwnProperty('valuesNext', 'value', valuesNext);
+  testRunner.assertEqual('valuesNext.value', '5', valuesNext.value);
+  testRunner.assertEqual('valuesNext.done', false, valuesNext.done);
+
+  valuesNext = values.next();
+  console.info('> valuesNext = values.next();');
+  testRunner.assertHasOwnProperty('valuesNext', 'done', valuesNext);
+  testRunner.assertHasOwnProperty('valuesNext', 'value', valuesNext);
+  testRunner.assertEqual('valuesNext.value', '6', valuesNext.value);
+  testRunner.assertEqual('valuesNext.done', false, valuesNext.done);
+
+  valuesNext = values.next();
+  console.info('> valuesNext = values.next();');
+  testRunner.assertHasOwnProperty('valuesNext', 'done', valuesNext);
+  testRunner.assertEqual('valuesNext.done', true, valuesNext.done);
+  console.groupEnd();
+
+  console.group('array.entries().reverse()');
+  const entriesReverse = array.entries().reverse();
+  console.info('> const entriesReverse = array.entries().reverse();');
+  testRunner.assertHasOwnProperty('entriesReverse', 'next', entriesReverse);
+  testRunner.assertHasOwnProperty('entriesReverse', Symbol.iterator, entriesReverse);
+
+  let entriesReverseNext = entriesReverse.next();
+  console.info('> let entriesReverseNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
+  testRunner.assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
+  testRunner.assertEqual('entriesReverseNext.value[0]', 2, entriesReverseNext.value[0]);
+  testRunner.assertEqual('entriesReverseNext.value[1]', '6', entriesReverseNext.value[1]);
+
+  entriesReverseNext = entriesReverse.next();
+  console.info('> entriesReverseNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
+  testRunner.assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
+  testRunner.assertEqual('entriesReverseNext.value[0]', 1, entriesReverseNext.value[0]);
+  testRunner.assertEqual('entriesReverseNext.value[1]', '5', entriesReverseNext.value[1]);
+
+  entriesReverseNext = entriesReverse.next();
+  console.info('> entriesReverseNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
+  testRunner.assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
+  testRunner.assertEqual('entriesReverseNext.value[0]', 0, entriesReverseNext.value[0]);
+  testRunner.assertEqual('entriesReverseNext.value[1]', '4', entriesReverseNext.value[1]);
+
+  entriesReverseNext = entriesReverse.next();
+  console.info('> entriesReverseNext = entries.next();');
+  testRunner.assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
+  testRunner.assertEqual('entriesReverseNext.done', true, entriesReverseNext.done);
+  console.groupEnd();
+
+  testRunner.assertEqual('Object.prototype.toString.call(new ReverseIterableArray())', '[object ReverseIterableArray]', Object.prototype.toString.call(new ReverseIterableArray()));
+  console.groupEnd();
+
+  testRunner.printResults();
+  console.groupEnd();
 }
 
-function assertEqual(expression, exptected, actual, message) {
-  const condition = exptected === actual;
-  console.info(`${expression}: ${actual} === ${exptected}? ${statusMark(condition)}`);
-  assert(condition, `Expect ${expression} to be ${exptected} but it was ${actual}.`);
-}
-
-function assertHasOwnProperty(expression, property, object) {
-  const condition = object.hasOwnProperty(property);
-  console.info(
-    `${expression}: ${expression}.hasOwnProperty(${property.toString()})? ${statusMark(condition)}`
-  );
-  assert(
-    condition,
-    `Expect ${expression} to have a property ${property.toString()} but it doesn’t.`
-  );
-}
-
-function statusMark(condition) {
-  return condition ? '✓' : '✗';
-}
-
-console.group('Tests');
-console.info('Running tests …');
-
-const array = new ReverseIterableArray('1', '2', '3');
-
-console.group('array[]');
-assertEqual('array.length', 3, array.length);
-assertEqual('array[0]', '1', array[0]);
-assertEqual('array[1]', '2', array[1]);
-assertEqual('array[2]', '3', array[2]);
-console.groupEnd();
-
-array.splice(0, array.length);
-
-console.group('Iterables & Iterators');
-array.push('4', '5', '6');
-
-console.group('array');
-assertHasOwnProperty('ReverseIterableArray.prototype', Symbol.iterator, ReverseIterableArray.prototype);
-console.groupEnd();
-
-console.group('array.entries()');
-const entries = array.entries();
-console.info('> const entries = array.entries();');
-assertHasOwnProperty('entries', 'next', entries);
-assertHasOwnProperty('entries', Symbol.iterator, entries);
-
-let entriesNext = entries.next();
-console.info('> let entriesNext = entries.next();');
-assertHasOwnProperty('entriesNext', 'done', entriesNext);
-assertHasOwnProperty('entriesNext', 'value', entriesNext);
-assertEqual('entriesNext.done', false, entriesNext.done);
-assertEqual('entriesNext.value[0]', 0, entriesNext.value[0]);
-assertEqual('entriesNext.value[1]', '4', entriesNext.value[1]);
-
-entriesNext = entries.next();
-console.info('> entriesNext = entries.next();');
-assertHasOwnProperty('entriesNext', 'done', entriesNext);
-assertHasOwnProperty('entriesNext', 'value', entriesNext);
-assertEqual('entriesNext.done', false, entriesNext.done);
-assertEqual('entriesNext.value[0]', 1, entriesNext.value[0]);
-assertEqual('entriesNext.value[1]', '5', entriesNext.value[1]);
-
-entriesNext = entries.next();
-console.info('> entriesNext = entries.next();');
-assertHasOwnProperty('entriesNext', 'done', entriesNext);
-assertHasOwnProperty('entriesNext', 'value', entriesNext);
-assertEqual('entriesNext.done', false, entriesNext.done);
-assertEqual('entriesNext.value[0]', 2, entriesNext.value[0]);
-assertEqual('entriesNext.value[1]', '6', entriesNext.value[1]);
-
-entriesNext = entries.next();
-console.info('> entriesNext = entries.next();');
-assertHasOwnProperty('entriesNext', 'done', entriesNext);
-assertEqual('entriesNext.done', true, entriesNext.done);
-console.groupEnd();
-
-console.group('array.keys()');
-const keys = array.keys();
-console.info('> const keys = array.keys();');
-assertHasOwnProperty('keys', 'next', keys);
-assertHasOwnProperty('keys', Symbol.iterator, keys);
-
-let keysNext = keys.next();
-console.info('> let keysNext = keys.next();');
-assertHasOwnProperty('keysNext', 'done', keysNext);
-assertHasOwnProperty('keysNext', 'value', keysNext);
-assertEqual('keysNext.value', 0, keysNext.value);
-assertEqual('keysNext.done', false, keysNext.done);
-
-keysNext = keys.next();
-console.info('> keysNext = keys.next();');
-assertHasOwnProperty('keysNext', 'done', keysNext);
-assertHasOwnProperty('keysNext', 'value', keysNext);
-assertEqual('keysNext.value', 1, keysNext.value);
-assertEqual('keysNext.done', false, keysNext.done);
-
-keysNext = keys.next();
-console.info('> keysNext = keys.next();');
-assertHasOwnProperty('keysNext', 'done', keysNext);
-assertHasOwnProperty('keysNext', 'value', keysNext);
-assertEqual('keysNext.value', 2, keysNext.value);
-assertEqual('keysNext.done', false, keysNext.done);
-
-keysNext = keys.next();
-console.info('> keysNext = keys.next();');
-assertHasOwnProperty('keysNext', 'done', keysNext);
-assertEqual('keysNext.done', true, keysNext.done);
-console.groupEnd();
-
-console.group('array.values()');
-const values = array.values();
-console.info('> const values = array.values();');
-assertHasOwnProperty('values', 'next', values);
-assertHasOwnProperty('values', Symbol.iterator, values);
-
-let valuesNext = values.next();
-console.info('> let valuesNext = values.next();');
-assertHasOwnProperty('valuesNext', 'done', valuesNext);
-assertHasOwnProperty('valuesNext', 'value', valuesNext);
-assertEqual('valuesNext.value', '4', valuesNext.value);
-assertEqual('valuesNext.done', false, valuesNext.done);
-
-valuesNext = values.next();
-console.info('> valuesNext = values.next();');
-assertHasOwnProperty('valuesNext', 'done', valuesNext);
-assertHasOwnProperty('valuesNext', 'value', valuesNext);
-assertEqual('valuesNext.value', '5', valuesNext.value);
-assertEqual('valuesNext.done', false, valuesNext.done);
-
-valuesNext = values.next();
-console.info('> valuesNext = values.next();');
-assertHasOwnProperty('valuesNext', 'done', valuesNext);
-assertHasOwnProperty('valuesNext', 'value', valuesNext);
-assertEqual('valuesNext.value', '6', valuesNext.value);
-assertEqual('valuesNext.done', false, valuesNext.done);
-
-valuesNext = values.next();
-console.info('> valuesNext = values.next();');
-assertHasOwnProperty('valuesNext', 'done', valuesNext);
-assertEqual('valuesNext.done', true, valuesNext.done);
-console.groupEnd();
-
-console.group('array.entries().reverse()');
-const entriesReverse = array.entries().reverse();
-console.info('> const entriesReverse = array.entries().reverse();');
-assertHasOwnProperty('entriesReverse', 'next', entriesReverse);
-assertHasOwnProperty('entriesReverse', Symbol.iterator, entriesReverse);
-
-let entriesReverseNext = entriesReverse.next();
-console.info('> let entriesReverseNext = entries.next();');
-assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
-assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
-assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
-assertEqual('entriesReverseNext.value[0]', 2, entriesReverseNext.value[0]);
-assertEqual('entriesReverseNext.value[1]', '6', entriesReverseNext.value[1]);
-
-entriesReverseNext = entriesReverse.next();
-console.info('> entriesReverseNext = entries.next();');
-assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
-assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
-assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
-assertEqual('entriesReverseNext.value[0]', 1, entriesReverseNext.value[0]);
-assertEqual('entriesReverseNext.value[1]', '5', entriesReverseNext.value[1]);
-
-entriesReverseNext = entriesReverse.next();
-console.info('> entriesReverseNext = entries.next();');
-assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
-assertHasOwnProperty('entriesReverseNext', 'value', entriesReverseNext);
-assertEqual('entriesReverseNext.done', false, entriesReverseNext.done);
-assertEqual('entriesReverseNext.value[0]', 0, entriesReverseNext.value[0]);
-assertEqual('entriesReverseNext.value[1]', '4', entriesReverseNext.value[1]);
-
-entriesReverseNext = entriesReverse.next();
-console.info('> entriesReverseNext = entries.next();');
-assertHasOwnProperty('entriesReverseNext', 'done', entriesReverseNext);
-assertEqual('entriesReverseNext.done', true, entriesReverseNext.done);
-console.groupEnd();
-
-assertEqual('Object.prototype.toString.call(new ReverseIterableArray())', '[object ReverseIterableArray]', Object.prototype.toString.call(new ReverseIterableArray()));
-console.groupEnd();
-
-console.info(
-  `Done. ${correctAssertions}/${totalAssertions} tests passed. ${
-    correctAssertions === totalAssertions ? '🎉🎉🎉' : ''
-  }`
-);
-console.groupEnd();
+tests();
